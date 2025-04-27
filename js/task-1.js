a952f01c2c528c1d5d4e95fdce82e8d47a93469d
Alona_Melnyk <melnukalenam2@gmail.com>
2025-04-27 16:35:34 +0200

    js1,2,2

--- js/task-1.js
+++ js/task-1.js
@@ -1,3 +1,12 @@
+function isEnoughCapacity(products, containerSize) {
+  const arrayProductsValue = Object.values(products);
+  let sumProductsValue = 0;
+  for (let value of arrayProductsValue) {
+    sumProductsValue += value;
+  }
+  return sumProductsValue < containerSize;
+}
+
 console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
 
 console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
